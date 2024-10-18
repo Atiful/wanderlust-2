4c0d2b7c2b1c3d7a816dbfc84ee77fec2e41e3c4
atiful haque <mdatifulhaque26@gmail.com>
2024-10-18 22:25:54 +0530

    added loader

---
+++ public/JS/script.js
@@ -0,0 +1,13 @@
+
+let btns = document.querySelectorAll("button");
+
+// btns.forEach((btn) => {
+//     btn.addEventListener("click" , (event) => {
+//         console.log("btn was clicked");
+//         btn.disabled = true;
+
+//         setTimeout(() => {
+//             btn.disabled = false;
+//         } , 1000);
+//     });
+// })
